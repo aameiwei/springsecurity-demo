3f62675849b4e54f39bcadc62561d973bd47b6be
aameiwei <923070686@qq.com>
2022-10-29 17:05:05 +0800

    20221029 rememberme功能，增加数据库配置

--- src/main/java/com/huo/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/com/huo/springsecuritydemo/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.huo.springsecuritydemo.config;
 import com.huo.springsecuritydemo.handle.MyAccessDeniedHandler;
 import com.huo.springsecuritydemo.handle.MyAuthenticationFailureHandler;
 import com.huo.springsecuritydemo.handle.MyAuthenticationSuccessHandler;
+import com.huo.springsecuritydemo.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +13,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
 
 /**
  * @Author 小源同学
@@ -24,6 +29,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     MyAccessDeniedHandler myAccessDeniedHandler;
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private PersistentTokenRepository persistentTokenRepository;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -78,6 +89,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.exceptionHandling()
                 .accessDeniedHandler(myAccessDeniedHandler);
 
+        //RememberMe功能实现
+        http.rememberMe()
+                //自定义登录逻辑
+                .userDetailsService(userDetailsService)
+                .tokenValiditySeconds(20) //1小时 记住我的时间(秒)，默认是14天
+                //持久层对象
+                .tokenRepository(persistentTokenRepository);
+
         //暂时理解为防火墙
         //关闭csrf防护
         http.csrf().disable();
@@ -87,4 +106,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder getPassWord() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        //自动建表，第一次启动会创建，再次起订记得关闭
+        //jdbcTokenRepository.setCreateTableOnStartup(true);
+        return jdbcTokenRepository;
+    }
+
 }
